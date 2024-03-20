415f5958903a6d774ab1657bbf867740c0a4c548
Tanner Saint <tsaint2004@gmail.com>
2024-03-20 22:34:57 +0000

    fixed loop

--- shell_loop.c
+++ shell_loop.c
@@ -1,7 +1,13 @@
-//gcc -Wall -Werror -Wextra -pedantic -std=gnu89 *.c -o hsh
+#include <stdio.h>
 
-void loop()
+/**
+ * compile command
+ * gcc -Wall -Werror -Wextra -pedantic -std=gnu89 *.c -o hsh
+ */
+
+int main()
 {
     char prompt[] = "shell$\n";
-    printf(prompt);
+    printf("%s", prompt);
+    return (0);
 }
